8fbf267cd0589a9f2c383eed378860317f8bde66
rtj226 <143648899+rtj226@users.noreply.github.com>
2025-11-24 12:16:25 -0500

    some functionality added

---
+++ script.js
@@ -0,0 +1,36 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+function goToSoldier() {
+    window.location.href = "storyPages/soldier.html"
+}
+
+function goToWife() {
+    window.location.href = "storyPages/wife.html"
+}
+
+function goToConscript() {
+    window.location.href = "storyPages/conscript.html"
+}
+
+function goToMother() {
+    window.location.href = "storyPages/mother.html"
+}
+
+function goToTeacher() {
+    window.location.href = "storyPages/teacher.html"
+}
+
+function goToJournalist() {
+    window.location.href = "storyPages/journalist.html"
+}
+
+function goToActivist() {
+    window.location.href = "storyPages/activist.html"
+}
---
+++ storyScript.js
@@ -0,0 +1,27 @@
+function goToSoldier() {
+    window.location.href = "soldier.html"
+}
+
+function goToWife() {
+    window.location.href = "wife.html"
+}
+
+function goToConscript() {
+    window.location.href = "conscript.html"
+}
+
+function goToMother() {
+    window.location.href = "mother.html"
+}
+
+function goToTeacher() {
+    window.location.href = "teacher.html"
+}
+
+function goToJournalist() {
+    window.location.href = "journalist.html"
+}
+
+function goToActivist() {
+    window.location.href = "activist.html"
+}
